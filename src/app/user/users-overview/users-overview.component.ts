f92719f8775e79a568cabd24bef3b5ed0f7d2cf0
jrnsmisrt <jeroen.smissaert@gmail.com>
2022-02-06 19:00:54 +0100

    build (user) added useroverview

---
+++ src/app/user/users-overview/users-overview.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from '@angular/core';
+import {collection, Firestore, getFirestore, getDocs,} from "@angular/fire/firestore";
+import {Observable} from "rxjs";
+import {User} from "../../model/User";
+import {UserService} from "../../services/user.service";
+
+@Component({
+  selector: 'app-users-overview',
+  templateUrl: './users-overview.component.html',
+  styleUrls: ['./users-overview.component.css']
+})
+export class UsersOverviewComponent implements OnInit {
+  db = getFirestore();
+  colRef = collection(this.db, 'users');
+  users: Observable<User[]> = this.userService.allUsers;
+
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit(): void {
+    console.log('lol');
+    this.lol();
+  }
+
+  lol() {
+    getDocs(this.colRef)
+      .then(((snapshot)=> {
+        console.log(snapshot.docs);
+
+      }));
+  }
+}
