2ddcaa29c24e7c5ac315749c9cccd9de897a7f5b
jrnsmisrt <jeroen.smissaert@gmail.com>
2022-02-11 17:09:46 +0100

    build (user-overview) added query getallusers to useroverview

--- src/app/user/users-overview/users-overview.component.ts
+++ src/app/user/users-overview/users-overview.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {collection, Firestore, getFirestore, getDocs,} from "@angular/fire/firestore";
+import {collection, Firestore, getFirestore, getDocs, orderBy, query, onSnapshot,} from "@angular/fire/firestore";
 import {Observable} from "rxjs";
 import {User} from "../../model/User";
 import {UserService} from "../../services/user.service";
+import {snapshotChanges} from "@angular/fire/compat/database";
 
 @Component({
   selector: 'app-users-overview',
@@ -19,15 +20,19 @@ export class UsersOverviewComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log('lol');
-    this.lol();
+    this.getAllUsers().then(()=>M.toast({html:'Viewing all users'}));
   }
 
-  lol() {
-    getDocs(this.colRef)
-      .then(((snapshot)=> {
-        console.log(snapshot.docs);
-
-      }));
+  async getAllUsers(){
+    let q = query(collection(this.db, 'users'), orderBy('lastname', 'asc'));
+    onSnapshot(q, snaps =>{
+      document.querySelector('#allusers')!.innerHTML = '';
+      snaps.forEach(doc=>{
+        const entry = document.createElement('p');
+        entry.textContent = doc.data()['firstname']+' '+ doc.data()['lastname'];
+        document.querySelector('#allusers')!.appendChild(entry);
+      })
+    });
   }
+
 }
