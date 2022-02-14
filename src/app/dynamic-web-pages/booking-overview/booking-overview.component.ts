0c28121e16a239796af7ce070790df6791b21afb
jrnsmisrt <jeroen.smissaert@gmail.com>
2022-02-14 19:02:05 +0100

    build (booking-overview)adjusted getAllBookins by userRole

--- src/app/dynamic-web-pages/booking-overview/booking-overview.component.ts
+++ src/app/dynamic-web-pages/booking-overview/booking-overview.component.ts
@@ -2,7 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {Booking} from "../../model/Booking";
 import {Observable} from "rxjs";
 import {AngularFirestore, AngularFirestoreCollection} from "@angular/fire/compat/firestore";
-import {getFirestore} from "@angular/fire/firestore";
+import {AuthService} from "../../services/auth.service";
+import {UserService} from "../../services/user.service";
 
 @Component({
   selector: 'app-booking-overview',
@@ -12,14 +13,28 @@ import {getFirestore} from "@angular/fire/firestore";
 export class BookingOverviewComponent implements OnInit {
   bookingCollection!: AngularFirestoreCollection<Booking>;
   bookings$!: Observable<Booking[]>;
+  isAdmin!: boolean;
 
-  constructor(private fireStore : AngularFirestore) {
+  constructor(private fireStore: AngularFirestore, private auth: AuthService, private userService: UserService) {
     this.bookingCollection = this.fireStore.collection('bookings');
+    this.setAdmin();
     this.getAllBookings();
+    this.fireStore.collection('bookings', ref => ref.where('uid', '==', this.auth.getUserUid())).valueChanges();
+
   }
 
-  private getAllBookings():Observable<Booking[]>{
-   return this.bookings$ = this.bookingCollection.valueChanges();
+  getAllBookings(): Observable<Booking[]> {
+    if (!this.isAdmin) {
+      return this.fireStore.collection<Booking>('bookings', ref => ref.where('uid', '==', this.auth.getUserUid())).valueChanges()
+    }
+    return this.bookings$ = this.bookingCollection.valueChanges();
+  }
+
+
+  private setAdmin() {
+    this.userService.currentUser.subscribe((user) => {
+      this.isAdmin = user.role === 'admin';
+    })
   }
 
   ngOnInit(): void {
