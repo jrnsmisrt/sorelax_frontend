c431f0e799510ad61df97e0baac428b6f37445fa
jrnsmisrt <jeroen.smissaert@gmail.com>
2022-02-22 07:45:21 +0100

    fix create-timeslot

---
+++ src/app/dynamic-web-pages/create-timeslot/create-timeslot.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CreateTimeslotComponent } from './create-timeslot.component';
+
+describe('CreateTimeslotComponent', () => {
+  let component: CreateTimeslotComponent;
+  let fixture: ComponentFixture<CreateTimeslotComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CreateTimeslotComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CreateTimeslotComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/dynamic-web-pages/create-timeslot/create-timeslot.component.ts
@@ -0,0 +1,80 @@
+import {Component, OnInit} from '@angular/core';
+import {AngularFirestore} from "@angular/fire/compat/firestore";
+import {FormBuilder, FormControl, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-create-timeslot',
+  templateUrl: './create-timeslot.component.html',
+  styleUrls: ['./create-timeslot.component.css']
+})
+export class CreateTimeslotComponent implements OnInit {
+  date!: string | number | string[] | undefined;
+  time!: string | number | string[] | undefined;
+
+  timeslotForm = this.formBuilder.group({
+    date: new FormControl('', [Validators.required]),
+    time: new FormControl('', [Validators.required])
+  })
+
+  constructor(private fireStore: AngularFirestore,
+              private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    $(document).ready(function () {
+      $('.datepicker').datepicker({
+        format: "dd/mm/yyyy",
+        //maxDate: new Date(new Date().getFullYear()+1),
+        minDate: new Date(Date.now())
+      });
+    });
+    $(document).ready(function () {
+      $('.timepicker').timepicker({
+        twelveHour: false
+      });
+    });
+    $(document).ready(function () {
+      $('.modal').modal();
+    });
+  }
+
+  createTimeslot() {
+    this.timeslotForm.patchValue({
+      date: this.date,
+      time: this.time
+    })
+    console.log(this.timeslotForm.get(['date'])?.value);
+    console.log();
+    console.log(this.time);
+    return this.fireStore.collection('timeslots').doc().set({
+      date: this.date,
+      time: this.time,
+      isAvailable: true,
+      confirmed: false
+    }).catch(error => {
+      console.log('timeslot form error', error);
+    })
+  }
+
+  openTimeslotModal() {
+    let timeslotModal = M.Modal.getInstance(document.querySelector('#timeslotModal')!);
+    timeslotModal.open();
+  }
+
+  confirmTimeslot() {
+    this.createTimeslot();
+  }
+
+  setDate() {
+    this.date = $('.datepicker').val();
+    this.timeslotForm.patchValue({
+      date: this.date
+    });
+  }
+
+  setTime() {
+    this.time = $('.timepicker').val();
+    this.timeslotForm.patchValue({
+      time: this.time
+    });  }
+}
