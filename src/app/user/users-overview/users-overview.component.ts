ffbb93037037d7e438240ea4a8b314b718e12c2d
jrnsmisrt <jeroen.smissaert@gmail.com>
2022-03-02 08:49:42 +0100

    Refactor user-overview

--- src/app/user/users-overview/users-overview.component.ts
+++ src/app/user/users-overview/users-overview.component.ts
@@ -3,6 +3,7 @@ import {collection, Firestore, getFirestore, getDocs, orderBy, query, onSnapshot
 import {Observable} from "rxjs";
 import {User} from "../../model/User";
 import {UserService} from "../../services/user.service";
+import {AngularFirestore} from "@angular/fire/compat/firestore";
 
 @Component({
   selector: 'app-users-overview',
@@ -10,12 +11,11 @@ import {UserService} from "../../services/user.service";
   styleUrls: ['./users-overview.component.css']
 })
 export class UsersOverviewComponent implements OnInit {
-  db = getFirestore();
-  colRef = collection(this.db, 'users');
-  users: Observable<User[]> = this.userService.getAllUsers();
+  users!: Observable<User[]>;
 
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private fireStore:AngularFirestore) {
+    this.users = this.fireStore.collection<User>('users').valueChanges();
   }
 
   ngOnInit(): void {
