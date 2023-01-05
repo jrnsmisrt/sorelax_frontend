bf2f66ebcc9d0668bd3b1b207945d69e2f55b3b8
jrnsmisrt <jeroen.smissaert@gmail.com>
2023-01-05 13:45:44 +0100

    Remove booking, add pregnancy warning

--- src/app/static-web-pages/home/home.component.ts
+++ src/app/static-web-pages/home/home.component.ts
@@ -14,9 +14,6 @@ export class HomeComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    this.initService.initSlider();
+   this.initService.initSlider();
   }
-
-
-
 }
