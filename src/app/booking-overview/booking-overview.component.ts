e8f13cf54884f35011b6c1b85bdc68d83083e657
jrnsmisrt <jeroen.smissaert@gmail.com>
2023-10-27 21:20:51 +0200

    cleanup bookingoverview

--- src/app/booking-overview/booking-overview.component.ts
+++ src/app/booking-overview/booking-overview.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Booking} from "../model/Booking";
-import {Observable} from "rxjs";
+import {Observable, Subject, takeUntil} from "rxjs";
 import {AngularFirestore, AngularFirestoreCollection} from "@angular/fire/compat/firestore";
 import {AuthService} from "../services/auth.service";
 import {UserService} from "../services/user.service";
@@ -12,29 +12,25 @@ import {InitService} from "../materialize/init.service";
   selector: 'app-booking-overview',
   templateUrl: './booking-overview.component.html',
 })
-export class BookingOverviewComponent implements OnInit {
+export class BookingOverviewComponent implements OnInit, OnDestroy {
   bookingCollection!: AngularFirestoreCollection<Booking>;
   bookings$!: Observable<Booking[]>;
-  booking$!: Observable<Booking | undefined>;
-
-  changeBookingStatusId: string | undefined;
-  changeBookingStatus: string | undefined;
-
   cancelBookingId!: string;
 
-  bookingUserFullName!: string;
   users$!: Observable<User[]>;
   user$!: Observable<User | undefined>;
 
   searchDate!: string;
   searchStatus!: string;
   searchMassage!: string;
+  private destroy$ = new Subject();
 
   constructor(private fireStore: AngularFirestore, private auth: AuthService, private userService: UserService, private init: InitService) {
     this.bookingCollection = this.fireStore.collection('bookings');
     this.bookings$ = this.getAllBookings();
     this.users$ = this.userService.allUsers;
-    this.user$ = this.fireStore.collection<User>('users').doc(`${firebase.auth().currentUser?.uid}`).valueChanges();
+    this.user$ = this.fireStore.collection<User>('users').doc(`${firebase.auth().currentUser?.uid}`)
+      .valueChanges().pipe(takeUntil(this.destroy$));
   }
 
   ngOnInit(): void {
@@ -44,38 +40,11 @@ export class BookingOverviewComponent implements OnInit {
   }
 
   getAllBookings(): Observable<Booking[]> {
-    return this.fireStore.collection<Booking>('bookings', ref => ref.where('userUid', '==', firebase.auth().currentUser?.uid)
-      .orderBy('date', 'asc').orderBy('time', 'asc').orderBy('requestedOn', 'asc')).valueChanges();
-  }
-
-  setBooking(bookingId: string) {
-    this.booking$ = this.fireStore.doc<Booking>(`bookings/${bookingId}`).valueChanges();
-  }
-
-  setUserName(userId: string | undefined) {
-    this.userService.getUser(userId).subscribe((user) => {
-      this.bookingUserFullName = user?.firstName + ' ' + user?.lastName!;
-    });
-  }
-
-  getUserName(id: string) {
-    this.fireStore.collection<User>('users').doc(id).valueChanges().subscribe((user) => {
-      this.bookingUserFullName += user?.firstName + ' ' + user?.lastName
-    })
-  }
-
-  confirmBooking(bookingId: string) {
-    let booking = this.fireStore.doc<Booking>(`bookings/${bookingId}`).valueChanges();
-    booking.subscribe((b) => {
-      if (b?.status !== 'confirmed') {
-        this.fireStore.doc<Booking>(`bookings/${bookingId}`).update({
-          status: 'confirmed'
-        }).catch(error => {
-          M.toast({html: `${error}`, classes: 'rounded red'});
-          console.log('confirm booking: ' + error);
-        })
-      }
-    })
+    return this.fireStore.collection<Booking>('bookings', ref =>
+      ref.where('userUid', '==', firebase.auth().currentUser?.uid)
+        .orderBy('date', 'asc').orderBy('time', 'asc')
+        .orderBy('requestedOn', 'asc'))
+      .valueChanges().pipe(takeUntil(this.destroy$));
   }
 
   cancelBooking(bookingId: string) {
@@ -83,9 +52,14 @@ export class BookingOverviewComponent implements OnInit {
       status: 'cancelled',
     }).then(async () => {
       M.toast({html: 'Boeking werd geannuleerd', classes: 'rounded teal'});
-      let booking = this.fireStore.collection<Booking>('bookings').doc(bookingId).valueChanges();
+
+      let booking = this.fireStore.collection<Booking>('bookings').doc(bookingId)
+        .valueChanges().pipe(takeUntil(this.destroy$));
       booking.subscribe((booking) => {
-        let usr = this.fireStore.collection<User>('users').doc(booking?.userUid).valueChanges();
+
+        let usr = this.fireStore.collection<User>('users').doc(booking?.userUid)
+          .valueChanges().pipe(takeUntil(this.destroy$));
+
         usr.subscribe((usr) => {
           this.fireStore.collection('mail').add({
             to: 'sverkouille@hotmail.com',
@@ -118,5 +92,8 @@ export class BookingOverviewComponent implements OnInit {
     M.Modal.getInstance(document.getElementById('confirmCancelBooking')!).open();
   }
 
-
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.complete();
+  }
 }
